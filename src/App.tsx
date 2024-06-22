c1731104c7c0d91f8ed03a95c3590acfe461f6b2
alanuchodevelop <kerosen07@outlook.es>
2024-06-22 00:56:30 -0600

    se agregan stilos y nuevos cambios con tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 
-
 function App() {
-
-
   return (
     <>
-
-        <h1 className="text-9xl">Hola mundo</h1>
+        <header className="bg-teal-400 py-5">
+           <h1 className="text-center text-4xl font-black">Calculadora de propinas y consumo</h1>
+        </header>
+        <main className="max-w-7xl mx-auto py-20 px-6 lg:px-8 grid md:grid-cols-2">
+            <div>
+                <h2>Menu</h2>
+            </div>
+            <div>
+                <h2>Consumo</h2>
+            </div>
+        </main>
     </>
   )
 }
