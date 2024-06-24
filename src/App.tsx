7f7c0c7e3240b6a0b1ef538b146993098f49123f
alanuchodevelop <kerosen07@outlook.es>
2024-06-23 20:50:15 -0600

    Se agregan  avances del boton de orden

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,31 @@
+import {menuItems} from "./data/db.ts";
+import MenuItem from "./components/MenuItem.tsx";
+import useOrder from "./hooks/useOrder.ts";
 
 function App() {
-  return (
-    <>
-        <header className="bg-teal-400 py-5">
-           <h1 className="text-center text-4xl font-black">Calculadora de propinas y consumo</h1>
-        </header>
-        <main className="max-w-7xl mx-auto py-20 px-6 lg:px-8 grid md:grid-cols-2">
-            <div>
-                <h2>Menu</h2>
-            </div>
-            <div>
-                <h2>Consumo</h2>
-            </div>
-        </main>
-    </>
-  )
+    const {addItem} = useOrder();
+      return (
+            <>
+                    <header className="bg-teal-400 py-5">
+                       <h1 className="text-center text-4xl font-black">Calculadora de propinas y consumo</h1>
+                    </header>
+                    <main className="max-w-7xl mx-auto py-20 px-6 lg:px-8 grid md:grid-cols-2">
+                        <div>
+                            <h2 className={"text-4xl font-black"}>Menu</h2>
+                            {menuItems.map((menuItem) => (
+                                <MenuItem
+                                key={menuItem.id}
+                                item={menuItem}
+                                addItem={addItem}
+                                />
+                            ))}
+                        </div>
+                        <div>
+                            <h2>Consumo</h2>
+                        </div>
+                    </main>
+            </>
+      )
 }
 
 export default App
